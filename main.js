81a5d05457af06d97ca96679691778fcea0cdd10
tino415 <cernakmartin3@gmail.com>
2015-03-02 15:45:58 +0100

    Implemented all, missing collisions snake -> wall, snake -> snake

--- main.js
+++ main.js
@@ -1,45 +1,130 @@
-var BLACK = '#000000';
-var WHITE = '#FFFFFF';
-var RED = '#FF0000';
-var P_SIZE = '15px';
-var CANVAS_HEIGHT = '512';
-var CANVAS_WIDTH = '1024';
-var UP = 0;
-var LEFT = 1;
-var DOWN = 2;
-var RIGHT = 3;
-
-var K_UP = 38;
-var K_RIGHT = 39;
-var K_DOWN = 40;
-var K_LEFT = 37;
-
 (function ( $ ) {
+    var UP = 0;
+    var LEFT = 1;
+    var DOWN = 2;
+    var RIGHT = 3;
+    
+    var K_UP = 38;
+    var K_RIGHT = 39;
+    var K_DOWN = 40;
+    var K_LEFT = 37;
 
-    $.fn.snakeGame = function() {
+    $.fn.snakeGame = function(options) {
 
-        this.addRect  = function(x, y, x_size, y_size, color) {
-            element = $('<div></div>')
+        act_size = 10
+        var settings = $.extend({
+            FPS : 30,
+            actor : {size : act_size},
+            move : {len : act_size},
+            snake : {
+                color : '#FFFFFF',
+                start : {
+                    x : 0,
+                    y : 0,
+                },
+                len : 1,
+                grow : 1,
+            },
+            food : {color : '#FF0000'},
+            background : '#000000',
+            scene : {
+                height : 512,
+                width : 1024,
+            }
+        }, options);
+
+        this.attr('tabindex', '0');
+
+        this.css({
+            'background' : settings.background,
+            'height' : settings.scene.height + 'px',
+            'width' : settings.scene.width + 'px',
+            'position' : 'relative',
+        })
+
+        function rand(dimension) {
+            len = settings.move.len;
+            return Math.round(Math.random() * dimension/len) * len;
+        }
+
+        function Element() {
+            element = $('<div></div>');
             element.css({
-                'position' : 'relative',
-                'top' : x + 'px',
-                'left' : y + 'px',
-                'background' : color,
-                'height' : x_size,
-                'width' : y_size,
+                'position' : 'absolute',
+                'height' : settings.actor.size,
+                'width' : settings.actor.size,
+                'top' : '0px',
+                'left' : '0px',
             })
-            this.append(element);
+            $.extend(element,{
+                x : 0,
+                y : 0,
+                update : function(x, y) {
+                    this.x = x || this.x;
+                    this.y = y || this.y;
+                    this.css({
+                        top : this.x + 'px',
+                        left : this.y + 'px',
+                    })
+                }
+            })
+            return element
+        }
+        
+        function Food() {
+            element = Element();
+            element.css('background', settings.food.color);
+            element.extend(element,{
+                regenerate : function() {
+                    this.x = rand(settings.scene.height);
+                    this.y = rand(settings.scene.width);
+                    console.log("Regenerating food possition" + this.x + ", " + this.y);
+                    this.update()
+                }
+            })
+            return element;
+        }
+        
+        function SnakeBlock(x, y) {
+            element = Element();
+            element.css('background', settings.snake.color);
+            element.update(x, y);
             return element;
         }
 
-        this.attr('tabindex', '0');
-        this.css({
-            'background' : BLACK,
-            'height' : CANVAS_HEIGHT + 'px',
-            'width' : CANVAS_WIDTH + 'px',
-        })
-        food = Food(this.addRect(20, 20, 10, 10, RED))
-        snake = Snake(this.addRect(20, 20, 10, 10, WHITE), food)
+        function Snake(scene) {
+            return {
+                y : settings.snake.start.x,
+                x : settings.snake.start.y,
+                body : [SnakeBlock()],
+                len : settings.snake.len,
+                scene : scene,
+                vector : RIGHT,
+                move : function() {
+                    switch(this.vector) {
+                        case UP: this.x -= settings.move.len; break;
+                        case RIGHT: this.y += settings.move.len; break;
+                        case DOWN: this.x += settings.move.len; break;
+                        case LEFT: this.y -= settings.move.len; break;
+                    }
+                    this.body.push(SnakeBlock(this.x, this.y));
+                    if(this.body.length > this.len) this.body.shift().remove();
+                    this.scene.append(this.body[this.body.length-1]);
+                }
+            }
+        }
+
+        this.append(snake = Snake(this), food = Food());
+        food.regenerate()
+        setInterval(function(){
+            snake.move()
+            if(snake.x == food.x && snake.y == food.y) {
+                console.log("Colision food");
+                snake.len += settings.snake.grow;
+                food.regenerate();
+            }
+        }, (1/settings.FPS)*1000);
+
         this.keydown(function(event) {
             switch(event.which) {
                 case K_UP: snake.vector = UP; break;
@@ -53,33 +138,3 @@ var K_LEFT = 37;
 
 }( jQuery ));
 
-function Food(element) {
-    element.setX = function(x) { this.css('top', x + 'px'); }
-    element.getX = function() { return parseFloat(this.css('top')); }
-    element.setY = function(y) { this.css('left', y + 'px'); }
-    element.getY = function() { return parseFloat(this.css('left')); }
-}
-
-function Snake(element, food) {
-    element.food = food
-    console.lof("asdafda")
-    element.setX = function(x) { this.css('top', x + 'px'); }
-    element.getX = function() { return parseFloat(this.css('top')); }
-    element.setY = function(y) { this.css('left', y + 'px'); }
-    element.getY = function() { return parseFloat(this.css('left')); }
-    element.vector = RIGHT;
-    element.move = function() {
-        if(this.food.getX() == this.getX() && this.food.getY() == this.getY()) {
-            food.setX((Math.random() * CANVAS_HEIGHT/P_SIZE) * P_SIZE);
-            food.getY((Math.random() * CANVAS_HEIGHT/P_SIZE) * P_SIZE);
-        }
-        switch(this.vector) {
-            case UP: this.setX(this.getX() - 10); break;
-            case RIGHT: this.setY(this.getY() + 10); break;
-            case DOWN: this.setX(this.getX() + 10); break;
-            case LEFT: this.setY(this.getY() - 10); break;
-        }
-    }
-    setInterval(function(){element.move()}, 512);
-    return element;
-}
