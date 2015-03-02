a9622052f2f626dfd9ee352298275f326c050b5d
tino415 <cernakmartin3@gmail.com>
2015-03-02 16:42:51 +0100

    JS snake self collision

--- main.js
+++ main.js
@@ -11,39 +11,36 @@
 
     $.fn.snakeGame = function(options) {
 
-        act_size = 10
-        var settings = $.extend({
-            FPS : 30,
-            actor : {size : act_size},
-            move : {len : act_size},
-            snake : {
-                color : '#FFFFFF',
-                start : {
-                    x : 0,
-                    y : 0,
-                },
-                len : 1,
-                grow : 1,
-            },
-            food : {color : '#FF0000'},
+        act_size = options.actor_size || 20;
+        var settings = {
+            FPS : 15,
+            actor_size : act_size,
+            move_len : act_size,
+            snake_color : '#FFFFFF',
+            snake_start_x : 0,
+            snake_start_y : 0,
+            snake_len : 1,
+            snake_grow : 2,
+            food_color : '#FF0000',
             background : '#000000',
-            scene : {
-                height : 512,
-                width : 1024,
-            }
-        }, options);
+            scene_height : 512,
+            scene_width : 1024,
+        };
+
+        scene_hmax = settings.scene_height - settings.actor_size;
+        scene_wmax = settings.scene_width - settings.actor_size;
 
         this.attr('tabindex', '0');
 
         this.css({
             'background' : settings.background,
-            'height' : settings.scene.height + 'px',
-            'width' : settings.scene.width + 'px',
+            'height' : settings.scene_height + 'px',
+            'width' : settings.scene_width + 'px',
             'position' : 'relative',
         })
 
         function rand(dimension) {
-            len = settings.move.len;
+            len = settings.move_len;
             return Math.round(Math.random() * dimension/len) * len;
         }
 
@@ -51,8 +48,8 @@
             element = $('<div></div>');
             element.css({
                 'position' : 'absolute',
-                'height' : settings.actor.size,
-                'width' : settings.actor.size,
+                'height' : settings.actor_size,
+                'width' : settings.actor_size,
                 'top' : '0px',
                 'left' : '0px',
             })
@@ -73,12 +70,11 @@
         
         function Food() {
             element = Element();
-            element.css('background', settings.food.color);
+            element.css('background', settings.food_color);
             element.extend(element,{
                 regenerate : function() {
-                    this.x = rand(settings.scene.height);
-                    this.y = rand(settings.scene.width);
-                    console.log("Regenerating food possition" + this.x + ", " + this.y);
+                    this.x = rand(scene_hmax);
+                    this.y = rand(scene_wmax);
                     this.update()
                 }
             })
@@ -87,50 +83,72 @@
         
         function SnakeBlock(x, y) {
             element = Element();
-            element.css('background', settings.snake.color);
+            element.css('background', settings.snake_color);
             element.update(x, y);
             return element;
         }
 
         function Snake(scene) {
             return {
-                y : settings.snake.start.x,
-                x : settings.snake.start.y,
+                y : settings.snake_start_x,
+                x : settings.snake_start_y,
                 body : [SnakeBlock()],
-                len : settings.snake.len,
+                len : settings.snake_len,
                 scene : scene,
                 vector : RIGHT,
                 move : function() {
                     switch(this.vector) {
-                        case UP: this.x -= settings.move.len; break;
-                        case RIGHT: this.y += settings.move.len; break;
-                        case DOWN: this.x += settings.move.len; break;
-                        case LEFT: this.y -= settings.move.len; break;
+                        case UP: this.x -= settings.move_len; break;
+                        case RIGHT: this.y += settings.move_len; break;
+                        case DOWN: this.x += settings.move_len; break;
+                        case LEFT: this.y -= settings.move_len; break;
+                    }
+                    console.log(this.colide(this.x, this.y));
+                    if(this.colide(this.x, this.y)) {
+                        console.log("Snake collided it self");
+                        return false;
                     }
                     this.body.push(SnakeBlock(this.x, this.y));
                     if(this.body.length > this.len) this.body.shift().remove();
                     this.scene.append(this.body[this.body.length-1]);
+                    return true;
+                },
+                colide : function(x, y) {
+                    for(i=0; i<this.body.length; i++) {
+                        if(this.body[i].x == x && this.body[i].y == y) {
+                            console.log("Colision detected");
+                            return true;
+                        }
+                    };
+                    return false;
                 }
             }
         }
 
         this.append(snake = Snake(this), food = Food());
         food.regenerate()
-        setInterval(function(){
-            snake.move()
+        gameLoop = setInterval(function(){
+            if(!snake.move()) clearInterval(gameLoop);
             if(snake.x == food.x && snake.y == food.y) {
-                console.log("Colision food");
-                snake.len += settings.snake.grow;
+                snake.len += settings.snake_grow;
                 food.regenerate();
             }
         }, (1/settings.FPS)*1000);
 
         this.keydown(function(event) {
             switch(event.which) {
-                case K_UP: snake.vector = UP; break;
-                case K_RIGHT: snake.vector = RIGHT; break;
-                case K_DOWN: snake.vector = DOWN; break;
-                case K_LEFT: snake.vector = LEFT; break;
+                case K_UP:
+                    if(snake.vector != DOWN) snake.vector = UP;
+                    break;
+                case K_RIGHT:
+                    if(snake.vector != LEFT) snake.vector = RIGHT;
+                    break;
+                case K_DOWN:
+                    if(snake.vector != UP) snake.vector = DOWN;
+                    break;
+                case K_LEFT:
+                    if(snake.vector != RIGHT) snake.vector = LEFT;
+                    break;
             }
             event.preventDefault()
         })
